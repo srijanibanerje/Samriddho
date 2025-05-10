24e9004fd333c9011492e246e2f407a48b39b916
srijanibanerje <srijaniofficial2022@gmail.com>
2025-05-10 14:12:42 +0530

    "done"

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -4,6 +4,8 @@ import pro2 from "../assets/images/products/2.jpeg"
 import pro3 from "../assets/images/products/3.jpeg"
 import pro4 from "../assets/images/products/4.jpeg"
 import pro5 from "../assets/images/products/5.jpeg"
+import pro6 from "../assets/images/products/6.jpeg"
+import pro7 from "../assets/images/products/7.jpeg"
 const Product = () => {
   return (
     <>
@@ -25,7 +27,7 @@ const Product = () => {
                                                         <h4 className="product-title"><a href="#" className="pr-name">Turmeric Powder, 100g</a></h4>
                                                         <div className="price ">
                                                             <ins><span className="price-amount"><span className="currencySymbol">Rs.</span>40.00</span></ins>
-                                                            <del><span className="price-amount"><span className="currencySymbol"></span>95.00</span></del>
+                                                            <del><span className="price-amount"><span className="currencySymbol">Rs.</span>95.00</span></del>
                                                         </div>
                                                         <div className="slide-down-box">
                                                             
@@ -50,8 +52,8 @@ const Product = () => {
                                                        
                                                         <h4 className="product-title"><a href="#" className="pr-name">Coriander Powder, 100g</a></h4>
                                                         <div class="price ">
-                                                            <ins><span class="price-amount"><span className="currencySymbol">Rs</span>40.00</span></ins>
-                                                            <del><span class="price-amount"><span className="currencySymbol">£</span>95.00</span></del>
+                                                            <ins><span class="price-amount"><span className="currencySymbol">Rs.</span>40.00</span></ins>
+                                                            <del><span class="price-amount"><span className="currencySymbol">Rs.</span>95.00</span></del>
                                                         </div>
                                                         <div className="slide-down-box">
                                                             
@@ -76,8 +78,8 @@ const Product = () => {
                                                        
                                                         <h4 className="product-title"><a href="#" className="pr-name">Garam Masala Powder, 100g</a></h4>
                                                         <div class="price ">
-                                                            <ins><span class="price-amount"><span className="currencySymbol">Rs</span>90.00</span></ins>
-                                                            <del><span class="price-amount"><span className="currencySymbol">£</span>95.00</span></del>
+                                                            <ins><span class="price-amount"><span className="currencySymbol">Rs.</span>90.00</span></ins>
+                                                            <del><span class="price-amount"><span className="currencySymbol">Rs.</span>95.00</span></del>
                                                         </div>
                                                         <div className="slide-down-box">
                                                             
@@ -104,8 +106,8 @@ const Product = () => {
                                                        
                                                         <h4 className="product-title"><a href="#" className="pr-name">Kasmiri Chili Powder, 100g</a></h4>
                                                         <div class="price ">
-                                                            <ins><span class="price-amount"><span className="currencySymbol">Rs</span>50.00</span></ins>
-                                                            <del><span class="price-amount"><span className="currencySymbol">£</span>95.00</span></del>
+                                                            <ins><span class="price-amount"><span className="currencySymbol">Rs.</span>50.00</span></ins>
+                                                            <del><span class="price-amount"><span className="currencySymbol">Rs.</span>95.00</span></del>
                                                         </div>
                                                         <div className="slide-down-box">
                                                             
@@ -131,7 +133,59 @@ const Product = () => {
                                                         <h4 className="product-title"><a href="#" className="pr-name">Jeera Powder, 100g</a></h4>
                                                         <div class="price ">
                                                             <ins><span class="price-amount"><span className="currencySymbol">Rs.</span>85.00</span></ins>
-                                                            <del><span class="price-amount"><span className="currencySymbol">£</span>95.00</span></del>
+                                                            <del><span class="price-amount"><span className="currencySymbol">Rs.</span>95.00</span></del>
+                                                        </div>
+                                                        <div className="slide-down-box">
+                                                            
+                                                            <div className="buttons">
+                                                               
+                                                                <a href="#" className="btn add-to-cart-btn"><i className="fa fa-cart-arrow-down" aria-hidden="true"></i>add to cart</a>
+                                                              
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+        </div>
+        <div className='col-lg-4 col-md-3 col-sm-6'>
+        <div className="contain-product layout-default">
+                                                    <div className="product-thumb">
+                                                        <a href="#" className="link-to-product">
+                                                            <img src={pro6} alt="Vegetables" width="370" height="270" className="product-thumnail"/>
+                                                        </a>
+                                                       
+                                                    </div>
+                                                    <div className="info" >
+                                                       
+                                                        <h4 className="product-title"><a href="#" className="pr-name">Kala Jeera, 100g</a></h4>
+                                                        <div class="price ">
+                                                            <ins><span class="price-amount"><span className="currencySymbol">Rs.</span>85.00</span></ins>
+                                                            <del><span class="price-amount"><span className="currencySymbol">Rs.</span>95.00</span></del>
+                                                        </div>
+                                                        <div className="slide-down-box">
+                                                            
+                                                            <div className="buttons">
+                                                               
+                                                                <a href="#" className="btn add-to-cart-btn"><i className="fa fa-cart-arrow-down" aria-hidden="true"></i>add to cart</a>
+                                                              
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+        </div>
+        <div className='col-lg-4 col-md-3 col-sm-6'>
+        <div className="contain-product layout-default">
+                                                    <div className="product-thumb">
+                                                        <a href="#" className="link-to-product">
+                                                            <img src={pro7} alt="Vegetables" width="370" height="270" className="product-thumnail"/>
+                                                        </a>
+                                                       
+                                                    </div>
+                                                    <div className="info" >
+                                                       
+                                                        <h4 className="product-title"><a href="#" className="pr-name">Black Pepper, 100g</a></h4>
+                                                        <div class="price ">
+                                                            <ins><span class="price-amount"><span className="currencySymbol">Rs.</span>85.00</span></ins>
+                                                            <del><span class="price-amount"><span className="currencySymbol">Rs.</span>95.00</span></del>
                                                         </div>
                                                         <div className="slide-down-box">
                                                             
